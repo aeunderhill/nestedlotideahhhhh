db2d9e65965cbc69fbaa837972d20becba5b8563
Adam Underhill <adameunderhill@gmail.com>
2021-05-24 16:15:44 -0700

    assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,9 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return `🟢🟢🟢Assertion Passed: [${actual}] === [${expected}]`
+  } else if (actual !== expected) {
+    return `🔴🔴🔴Assertion Failed: [${actual}] !== [${expected}]`
+  }
+};
+
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"))
